d1efea00b34f09627fc90f35e3239991d7f7274b
anan <anansaleh18@gmail.com>
2022-12-30 12:54:33 +0200

    designed an album page with material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,89 @@
 import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
+
+import useStyles from './styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const App = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <AppBar position='relative'>
+                <Toolbar>
+                    <PhotoCamera className={classes.icon}/>
+                    <Typography variant='h6'> 
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div className={classes.container}>
+                    <Container maxWidth='sm'>
+                        <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+                            Photo Album
+                        </Typography>
+                        <Typography variant='h5' align='center' color='textSecondary' paragraph>
+                            Hello everyone This is a photo album and I'm trying to make this sentence as long as possible s owe can see how does it look like on screen
+                        </Typography>
+                        <Typography>
+                        <div className={classes.buttons}>
+                            <Grid container spacing={2} justifyContent='center'>
+                                <Grid item>
+                                    <Button variant='contained' color='primary'>
+                                        See my photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant='outlined' color='primary'>
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                        </Typography>
+                    </Container>
+                </div>
+                <Container className={classes.cardGrid} maxWidth='md'>
+                    <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                        <Card className={classes.card}>
+                            <CardMedia 
+                                className={classes.CardMedia}
+                                image="https://source.unsplash.com/random"    
+                                title="Image title"
+                            />
+                            <CardContent className={classes.cardContent}>
+                                <Typography gutterBottom variant='h5'>
+                                    Heading
+                                </Typography>
+                                <Typography>
+                                    This is a meda card. You can use this section to describve the content.
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size='small' color='primary'>View</Button>
+                                <Button size='small' color='primary'>Edit</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    ))}
+
+                    </Grid>
+                </Container>
+            </main>
+            <footer className={classes.footer}>
+                <Typography variant='h6' align='center' gutterBottom>
+                    Footer
+                </Typography>
+                <Typography variant='subtitle1' align='center' color='textSeconday'>
+                    Something here to give the footer a purpose!
+                </Typography>
+            </footer>
+        </>
     )
 }
 
